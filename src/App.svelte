3e5e2aced8fbc4dd93b082ee5615daad04f3c2df
Anu Lama <anulama@u.boisestate.edu>
2022-03-03 18:12:47 -0700

    Make some components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+  import FeedbackList from './components/FeedbackList.svelte';
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'lorem ipsum dolor sit amet consectetur adipiscing elit. consequuntur vel vitae commidi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'lorem ipsum dolor sit amet consectetur adipiscing elit. consequuntur vel vitae commidi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'lorem ipsum dolor sit amet consectetur adipiscing elit. consequuntur vel vitae commidi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+  <FeedbackList {feedback} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
